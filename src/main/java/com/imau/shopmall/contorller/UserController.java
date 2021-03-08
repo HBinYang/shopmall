6190b6fe70c96e960bba238c893a6ec23374e4bc
yhaobin1 <yanghaobin1999@163.com>
2021-03-08 17:19:34 +0800

    配置了视图解析器

--- src/main/java/com/imau/shopmall/contorller/UserController.java
+++ src/main/java/com/imau/shopmall/contorller/UserController.java
@@ -2,14 +2,15 @@ package com.imau.shopmall.contorller;
 
 import com.imau.shopmall.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author HBYang
  * @date 2021/3/5 16:34
  */
-@RestController//返回json格式的数据
+
+@Controller
 @RequestMapping("/user")
 public class UserController {
     @Autowired
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Admin
+  Date: 2021/3/5
+  Time: 16:32
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <p>Hello Spring boot !</p>
+    <h1>Hello SSM</h1>
+</body>
+</html>
